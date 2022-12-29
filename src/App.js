f1c3f3773c58e144820579c212c0bad60f7cc06d
Nahuel Genchi <65604505+NahuelGenchi@users.noreply.github.com>
2022-12-28 23:40:52 -0300

    Creating Basic Exercise 003

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { myFunction } from "./exercises/basic/002/index";
+import { myFunction } from "./exercises/basic/003/index";
 
 import "./scss/main.scss";
 
---
+++ src/exercises/basic/003/index.js
@@ -0,0 +1,10 @@
+/*
+Write a JavaScript program to get the current date.
+
+Expected Output:
+mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
+*/
+
+export const myFunction = () => {
+  console.log("Hello, World!");
+};
