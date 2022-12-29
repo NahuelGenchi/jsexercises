46f81bd4e98c0fab633e2e96ac5f2d12c2b53a69
Nahuel Genchi <65604505+NahuelGenchi@users.noreply.github.com>
2022-12-28 23:45:38 -0300

    Basic Exercise 003 done

--- src/exercises/basic/003/index.js
+++ src/exercises/basic/003/index.js
@@ -5,6 +5,17 @@ Expected Output:
 mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
 */
 
+const date = new Date();
+
+let [dayMonth, month, year] = [date.getDate(), date.getMonth(), date.getFullYear()];
+
+if (dayMonth < 10) {
+  dayMonth = `0${dayMonth}`;
+};
+if (month < 10) {
+  month = `0${month}`;
+};
+
 export const myFunction = () => {
-  console.log("Hello, World!");
+  console.log(`${dayMonth}/${month}/${year}`);
 };
