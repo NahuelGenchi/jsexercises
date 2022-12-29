324ba4241dc20bfe8b166f345daab363c12874b3
Nahuel Genchi <65604505+NahuelGenchi@users.noreply.github.com>
2022-12-28 23:33:03 -0300

    Creating Basic 002 exercise

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { myFunction } from "./exercises/basic/001/index";
+import { myFunction } from "./exercises/basic/002/index";
 
 import "./scss/main.scss";
 
---
+++ src/exercises/basic/002/index.js
@@ -0,0 +1,7 @@
+/*
+Write a JavaScript program to print the contents of the current window.
+*/
+
+export const myFunction = () => {
+  console.log("Hello World!");
+};
