ebf51b664bd7a9e7d82ce324c2806e7fb6f9ebf6
Nahuel Genchi <65604505+NahuelGenchi@users.noreply.github.com>
2022-12-28 23:48:53 -0300

    Creating Basic Exercise 004

---
+++ src/exercises/basic/004/index.js
@@ -0,0 +1,7 @@
+/*
+Write a JavaScript program to find the area of a triangle where lengths of the three of its sides are 5, 6, 7.
+*/
+
+export const myFunction = () => {
+  console.log("Hello, World!");
+};
