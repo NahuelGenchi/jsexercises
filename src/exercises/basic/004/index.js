cb87f734d16a9180857ae6e5575b8d881d6b64f7
Nahuel Genchi <65604505+NahuelGenchi@users.noreply.github.com>
2022-12-29 00:13:11 -0300

    Exercise Basic 004 done

--- src/exercises/basic/004/index.js
+++ src/exercises/basic/004/index.js
@@ -2,6 +2,15 @@
 Write a JavaScript program to find the area of a triangle where lengths of the three of its sides are 5, 6, 7.
 */
 
+const findArea = (a, b, c) => {
+  const findSemiParameter = (a2, b2, c2) => {
+    return (a + b + c) / 2;
+  };
+  const s = findSemiParameter(a, b, c);
+  const area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+  return area;
+};
+
 export const myFunction = () => {
-  console.log("Hello, World!");
+  console.log(findArea(5, 6, 7));
 };
