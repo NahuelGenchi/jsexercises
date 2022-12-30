64260019d6c602079701bcfd637049454a8a377c
Nahuel Genchi <65604505+NahuelGenchi@users.noreply.github.com>
2022-12-29 21:00:01 -0300

    Basic Exercise 005 done

--- src/exercises/basic/005/index.js
+++ src/exercises/basic/005/index.js
@@ -4,6 +4,17 @@ in right direction by periodically removing one letter from
 the end of the string and attaching it to the front.
 */
 
+const reverseString = (str) => {
+  let reversedStr = [];
+  for (let i = 0; i < str.length; i++) {
+    reversedStr.unshift(str[i]);
+  };
+  reversedStr = reversedStr.join("");
+
+  console.log(`Your string: ${str}`);
+  console.log(`Your string reversed: ${reversedStr}`);
+};
+
 export const myFunction = () => {
-  console.log("Hello, World!");
+  reverseString("Nahuel");
 };
