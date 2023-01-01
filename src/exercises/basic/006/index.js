2c96837947da31a64e0b0d59c27f71285a42553b
Nahuel Genchi <65604505+NahuelGenchi@users.noreply.github.com>
2022-12-31 23:10:15 -0300

    Exercise Basic 006 done

--- src/exercises/basic/006/index.js
+++ src/exercises/basic/006/index.js
@@ -3,6 +3,32 @@ Write a JavaScript program to determine whether
 a given year is a leap year in the Gregorian calendar.
 */
 
+const determineLeapYear = (year) => {
+  let isLeapYear;
+
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        isLeapYear = true;
+      } else {
+        isLeapYear = false;
+      };
+    } else {
+      isLeapYear = true;
+    };
+  } else {
+    isLeapYear = false;
+  };
+
+  if (isLeapYear) {
+    console.log(`${year} is a leap year.`);
+  } else {
+    console.log(`${year} is not a leap year.`);
+  };
+};
+
 export const myFunction = () => {
-  console.log("Hello, World!");
+  determineLeapYear(1992);
+  determineLeapYear(2000);
+  determineLeapYear(1900);
 };
