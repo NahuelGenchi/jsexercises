71dd2a78b67916e7789b4a8012f568a2fe373397
Nahuel Genchi <65604505+NahuelGenchi@users.noreply.github.com>
2023-01-03 23:13:41 -0300

    Exercise Basic 008 done

--- src/exercises/basic/008/index.js
+++ src/exercises/basic/008/index.js
@@ -6,6 +6,18 @@ If the user input matches with guess number, the program will display
 a message "Good Work" otherwise display a message "Not matched".
 */
 
+const guessNumberGame = () => {
+  const getRandomNumber = Math.floor(Math.random() * 10) + 1;
+  const userNumber = Number(prompt("Choose a number between 1 to 10"));
+  console.log(`Guess number is ${getRandomNumber} and your number is ${userNumber}`);
+
+  if (getRandomNumber === userNumber) {
+    console.log("Good Work.");
+  } else {
+    console.log("Not matched")
+  };
+};
+
 export const myFunction = () => {
-  console.log("Hello, World!");
+  guessNumberGame();
 };
