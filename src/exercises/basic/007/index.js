52169f178b2454f2dbe8117910e48d7b94d58dac
Nahuel Genchi <65604505+NahuelGenchi@users.noreply.github.com>
2023-01-03 22:59:33 -0300

    Exercise Basic 007 done

--- src/exercises/basic/007/index.js
+++ src/exercises/basic/007/index.js
@@ -2,6 +2,17 @@
 Write a JavaScript program to find 1st January is being a Sunday between 2014 and 2050.
 */
 
+const getFirstDayOfYear = () => {
+  for (let i = 2014; i <= 2050; i++) {
+    const date = new Date(i, 0, 1);
+    const day = date.getDay();
+
+    if (day === 0) {
+      console.log(`1st January of ${i} is Sunday!`);
+    };
+  };
+};
+
 export const myFunction = () => {
-  console.log("Hello, World!");
+  getFirstDayOfYear();
 };
